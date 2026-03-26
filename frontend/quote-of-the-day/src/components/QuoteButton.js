c376af2e8776e7f916c759f43f8f946700226345
TBrugarolas <tbrugarolas31@gmail.com>
2026-03-26 12:27:42 -0400

    finished

--- frontend/quote-of-the-day/src/components/QuoteButton.js
+++ frontend/quote-of-the-day/src/components/QuoteButton.js
@@ -1,19 +1,30 @@
 import { useDispatch } from 'react-redux';
 
-
 function QuoteButton() {
-  const nextQuote = "This is a new quote!";
   const dispatch = useDispatch();
 
-  const newQuote = () => {
-    dispatch({
-      type: 'NEW_QUOTE',
-      payload: { text: nextQuote }
-    });
+  const newQuote = async () => {
+    try {
+      const response = await fetch(
+        "https://react-quotes-9wiq.onrender.com/api/quote"
+      );
+
+      const data = await response.json();
+
+      dispatch({
+        type: 'NEW_QUOTE',
+        payload: { text: data.quote }
+      });
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
   };
 
   return (
-    <button className="btn btn-primary justify-content-center" onClick={newQuote}>
+    <button
+      className="btn btn-primary justify-content-center"
+      onClick={newQuote}
+    >
       Get New Quote
     </button>
   );
